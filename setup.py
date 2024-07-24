2953a833f342c3e2c37380fe251eeee51180a18a
Shaun Donnelly <shaun.t.e.donnelly@gmail.com>
2024-07-24 15:20:44 +0100

    Updated versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="BasisOpt",
-    version='1.1',
+    version='1.2',
     packages=find_packages(),
     install_requres=[
         'numpy',
